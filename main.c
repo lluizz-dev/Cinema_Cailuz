dd1534effdbc3a30fff286870fffed68f81fb087
Luiz Eduardo <luiz.eduardodss24@gmail.com>
2025-08-06 09:37:31 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void voltarMenu();
+void listaFilmes();
 
 int main()
 {
@@ -13,13 +17,13 @@ int main()
         printf("4. Cancelar reserva.\n");
         printf("5. Sair.\n\n");
         
-        printf("Escolha uma opção: \n");
+        printf("Escolha uma opção: ");
         scanf("%d", &opcao);
         
         switch (opcao) {
             case 1: 
-                // função da lista de fimes em Cartaz
-                break;
+                listaFilmes();
+                voltarMenu(&opcao);
             case 2:
                 // função que reserva os assento (CREATE)
                 break;
@@ -34,9 +38,37 @@ int main()
                 break;
             default: 
                 printf("Opção inválida!\n");
-                printf("Digite novamente! \n");
+                printf("Digite novamente!\n");
         }
     } while (opcao < 1 || opcao > 5);
+}
 
-    return 0;
+void voltarMenu(int *Popcao) {
+    *Popcao = 100;
+    while (*Popcao > 1 || *Popcao < 0) {
+        printf("Deseja voltar ao menu?\n");
+        printf("0 - Não / 1 - Sim\n");
+        scanf("%d", Popcao);
+        
+        if (*Popcao > 1 || *Popcao < 0) {
+            printf("Opção inválida.\n Tente novamente!\n");
+        }
+    }
+    
+    if (*Popcao == 1) {
+        *Popcao = 0;
+    }
+    else if (*Popcao == 0) {
+        *Popcao = 1;
+    }
+    system("clear");
+}
+
+void listaFilmes() {
+    system("clear");
+    printf("1. Quarteto Fantástico.\n");
+    printf("2. Superman.\n");
+    printf("3. Smurfs.\n");
+    printf("4. Lilo & Stitch.\n");
+    printf("5. Como Treinar O Seu Dragão.\n\n");
 }
